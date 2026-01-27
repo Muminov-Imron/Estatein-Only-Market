10232140acf69da58a7aaed1eaab3f9bad881e28
abdulboriy qoraxojayev <abdulboriyqoraxojayev@MacBook-Air--imron.local>
2026-01-27 19:50:29 +0500

    comintriya

--- styles/script.js
+++ styles/script.js
@@ -2,12 +2,10 @@ const items = document.querySelectorAll(".nav-item");
 const bg = document.querySelector(".nav-bg");
 
 function updateBg(el) {
-  // Fonning kengligi va o'rnini tugmaga qarab belgilaymiz
   bg.style.width = `${el.offsetWidth}px`;
   bg.style.left = `${el.offsetLeft}px`;
 }
 
-// Sahifa yuklanganda "Home" (active) ustida bo'lishi uchun
 const activeItem = document.querySelector(".nav-item.active");
 updateBg(activeItem);
 
@@ -15,11 +13,25 @@ items.forEach((item) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
 
-    // Eski active klassini olib tashlash va yangisiga qo'shish
     items.forEach((i) => i.classList.remove("active"));
     item.classList.add("active");
 
-    // Fonni surish
     updateBg(item);
   });
+});
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+
+  sidebar.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (!sidebar.contains(e.target) && !menuBtn.contains(e.target)) {
+    sidebar.classList.remove("active");
+    menuBtn.classList.remove("open");
+  }
 });
